cee1539ac07c4b2516a95b910f839d6445bedd42
Alex Hill <big_al_hill@comcast.net>
2017-06-13 17:45:34 -0600

    React day2 project

--- src/Stage 4/models/Employee.js
+++ src/Stage 4/models/Employee.js
@@ -1,9 +1,24 @@
 export default class Employee {
-  // constructor
-  
-  // updateName
+  constructor(id, name, phone, title){
 
-  // updatePhone
+    this.id = id;
 
-   // updateTitle
+    this.name = name;
+
+    this.phone= phone;
+
+   this.title = title;
+  }
+
+  updateName(name) {
+    this.name = name;
+  }
+
+  updatePhone(phone){
+    this.phone = phone;
+  }
+
+  updateTitle(title) {
+    this.title = title;
+  }
 }
